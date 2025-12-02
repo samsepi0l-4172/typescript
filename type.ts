8bc814938f44de96d647c1cb6cb9c757a2d941ab
samsepi0l-4172 <89460136+samsepi0l-4172@users.noreply.github.com>
2025-12-02 12:05:03 +0900

    3.4.0

--- type.ts
+++ type.ts
@@ -14,14 +14,14 @@
 // superPrint([1, 2, true, false]);
 
 type SuperPrint = {
-  <T>(arr: T[]): T;
+  <T, V>(arr: T[], b: V): T;
 };
 
 const superPrint: SuperPrint = (arr) => arr[0];
 
-const a = superPrint([1, 2, 3, 4]);
-const b = superPrint([true, false, true]);
-const c = superPrint(['a', 'b', 'c']);
-const d = superPrint([1, 2, true, false]);
+const a = superPrint([1, 2, 3, 4], 'x');
+const b = superPrint([true, false, true], 1);
+const c = superPrint(['a', 'b', 'c'], false);
+const d = superPrint([1, 2, true, false], []);
 
 console.log(a, b, c, d);
